103aadbff49c10a56045e4ed75550142d71720ab
sotsoguk <sotsoguk@gmail.com>
2019-04-15 10:55:12 +0200

    PE20

--- pe20/pe20.cpp
+++ pe20/pe20.cpp
@@ -7,22 +7,28 @@ typedef std::vector<int> bigNum;
 
 bigNum bigFactorial(int maxNum)
 {
-	bigNum result = {1};
-	result.reserve(3000);
-	for (int i=2;i<=maxNum;i++)
+	// bigNum result = {1};
+	// result.reserve(3000);
+	bigNum result = bigNum(200,0);
+	result[0] = 1;
+	for (int i = 2; i <= maxNum; i++)
 	{
 		int carry = 0;
-		for (auto& d:result){
+		for (auto &d : result)
+		{
 			d = d * i + carry;
-			if (d >= 10){
+			if (d >= 10)
+			{
 				carry = d / 10;
 				d %= 10;
 			}
-			else {
+			else
+			{
 				carry = 0;
 			}
 		}
-		while (carry > 0){
+		while (carry > 0)
+		{
 			result.push_back(carry % 10);
 			carry /= 10;
 		}
@@ -30,12 +36,13 @@ bigNum bigFactorial(int maxNum)
 	return result;
 }
 
-int main(int argc, char *argv[]) {
-	bigNum test = bigFactorial(100);
+int main(int argc, char *argv[])
+{
+	bigNum test = bigFactorial(1000);
 	//for (auto i=test.rbegin();i != test.rend();i++)
 	//	std::cout << *i;
 	int64_t sum = 0;
-	for (auto &d:test)
-		sum +=d;
+	for (auto &d : test)
+		sum += d;
 	std::cout << sum << endl;
 }
