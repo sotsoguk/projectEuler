932979a2b3438338a1fa9a2ec8eee5af4b07c5e2
sotsoguk <sotsoguk@gmail.com>
2019-04-15 08:43:05 +0200

    PE 20

---
+++ pe20/pe20.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef std::vector<int> bigNum;
+
+bigNum bigFactorial(int maxNum)
+{
+	bigNum result = {1};
+	result.reserve(3000);
+	for (int i=2;i<=maxNum;i++)
+	{
+		int carry = 0;
+		for (auto& d:result){
+			d = d * i + carry;
+			if (d >= 10){
+				carry = d / 10;
+				d %= 10;
+			}
+			else {
+				carry = 0;
+			}
+		}
+		while (carry > 0){
+			result.push_back(carry % 10);
+			carry /= 10;
+		}
+	}
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	bigNum test = bigFactorial(100);
+	//for (auto i=test.rbegin();i != test.rend();i++)
+	//	std::cout << *i;
+	int64_t sum = 0;
+	for (auto &d:test)
+		sum +=d;
+	std::cout << sum << endl;
+}
